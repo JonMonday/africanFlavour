1f0da2dfe5f1750c994f0d0c33df8e9e50da4b92
808-cmd <monday005john@gmail.com>
2022-01-20 03:02:34 +0000

    second

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
-const burger = document.getElementByClass("");
+document.addEventListener("click" , e=>{
+
+    const burger = document.getElementById('burger');
+    const navLink = document.getElementById('nav-link');
+
+    if (e.target === burger) {
+        linkToggle();
+    }
+        
+    function linkToggle(){
+        if(navLink.classList.length === 0 || navLink.classList.contains('disable')){
+            navLink.classList.remove('disable');
+            navLink.classList.add('active');        
+        }
+        else if (navLink.classList.contains('active')) {
+            navLink.classList.remove('active');
+            navLink.classList.add('disable');
+        }
+        
+    }
+
+
+})
