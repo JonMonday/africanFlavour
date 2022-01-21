a3238867f69e317396ce0b6e519c75e421c79e4d
808-cmd <monday005john@gmail.com>
2022-01-20 20:36:09 -1200

    third

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("click" , e=>{
 
     const burger = document.getElementById('burger');
     const navLink = document.getElementById('nav-link');
+    const firstArticle = document.getElementById('first-article');
 
     if (e.target === burger) {
         linkToggle();
@@ -10,11 +11,15 @@ document.addEventListener("click" , e=>{
     function linkToggle(){
         if(navLink.classList.length === 0 || navLink.classList.contains('disable')){
             navLink.classList.remove('disable');
-            navLink.classList.add('active');        
+            navLink.classList.add('active');   
+            firstArticle.classList.add('srink-article');
+            firstArticle.classList.remove('expand-article');     
         }
         else if (navLink.classList.contains('active')) {
             navLink.classList.remove('active');
             navLink.classList.add('disable');
+            firstArticle.classList.remove('srink-article');
+            firstArticle.classList.add('expand-article');  
         }
         
     }
