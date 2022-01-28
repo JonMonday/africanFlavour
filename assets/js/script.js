3f708f1983bbc144e9fc66636aa7786c6f025ad4
808-cmd <monday005john@gmail.com>
2022-01-28 11:40:23 +0000

    making things make sense

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
-document.addEventListener("click" , e=>{
+function menuToggle() {
+    var menuButton = document.getElementById('menu');
+    var nav = document.getElementById('nav');
+    var top = document.getElementById('top');
+    if(menuButton.classList.contains('Notclicked')){
+        menuButton.classList.remove('Notclicked');
+        menuButton.classList.add('clicked');
 
-    const burger = document.getElementById('burger');
-    const navLink = document.getElementById('nav-link');
-    const firstArticle = document.getElementById('first-article');
+        top.classList.add('hidden');
+        top.classList.remove('visible');
 
-    if (e.target === burger) {
-        linkToggle();
+        nav.classList.add('visibleMenu');
+        nav.classList.remove('hiddenMenu');
+
+        menuButton.innerText = 'Close';
     }
-        
-    function linkToggle(){
-        if(navLink.classList.length === 0 || navLink.classList.contains('disable')){
-            navLink.classList.remove('disable');
-            navLink.classList.add('active');   
-            firstArticle.classList.add('srink-article');
-            firstArticle.classList.remove('expand-article');     
-        }
-        else if (navLink.classList.contains('active')) {
-            navLink.classList.remove('active');
-            navLink.classList.add('disable');
-            firstArticle.classList.remove('srink-article');
-            firstArticle.classList.add('expand-article');  
-        }
-        
+    else if (menuButton.classList.contains('clicked')){
+        menuButton.classList.remove('clicked');
+        menuButton.classList.add('Notclicked');
+
+        top.classList.add('visible');
+        top.classList.remove('hidden');
+
+        nav.classList.add('hiddenMenu');
+        nav.classList.remove('visibleMenu');
+
+        menuButton.innerText = 'Menu';
     }
-
-
-})
+}
